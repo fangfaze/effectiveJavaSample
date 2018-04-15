3bc5ab19beee28bd3d83e7dabc7c6316e8cbc849
chenze <ace50@163.com>
2018-04-15 12:24:06 +0800

    rule16

---
+++ src/main/java/com/fangfaze/java/study/chaptere4/CompositionHashSet.java
@@ -0,0 +1,33 @@
+package com.fangfaze.java.study.chaptere4;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class CompositionHashSet<E> {
+
+    private int addCount = 0;
+    private final HashSet<E> hashSet;
+
+    public CompositionHashSet() {
+        hashSet = new HashSet<>();
+    }
+
+    public CompositionHashSet(int initCap, float loadFactor) {
+        hashSet = new HashSet<>(initCap, loadFactor);
+    }
+
+    public boolean add(E e) {
+        addCount++;
+        return hashSet.add(e);
+    }
+
+    public boolean addAll(Collection<? extends E> c) {
+        addCount += c.size();
+        return hashSet.addAll(c);
+    }
+
+    public int getAddCount() {
+        return addCount;
+    }
+
+}
--- src/test/java/com/fangfaze/java/study/chapter4/Rule16Test.java
+++ src/test/java/com/fangfaze/java/study/chapter4/Rule16Test.java
@@ -1,5 +1,6 @@
 package com.fangfaze.java.study.chapter4;
 
+import com.fangfaze.java.study.chaptere4.CompositionHashSet;
 import com.fangfaze.java.study.chaptere4.InheritanceHashSet;
 import org.junit.Test;
 
@@ -8,14 +9,23 @@ import java.util.Arrays;
 
 public class Rule16Test {
     /**
-     * 第16条 符合优先于继承
+     * 第16条 复合优先于继承
      */
+    // 复合类有时需要
+    // 在回调
+
+
+
     @Test
     public void testExample16() {
-        InheritanceHashSet<String> set = new InheritanceHashSet<>();
-        set.addAll(Arrays.asList("A", "B", "C"));
+        InheritanceHashSet<String> set1 = new InheritanceHashSet<>();
+        set1.addAll(Arrays.asList("A", "B", "C"));
+        System.out.println(set1.getAddCount());
 
-        System.out.println(set.getAddCount())k;
+        CompositionHashSet<String> set2 = new CompositionHashSet<>();
+        set2.addAll(Arrays.asList("A", "B", "C"));
+        System.out.println(set2.getAddCount());
+
 
 
     }
